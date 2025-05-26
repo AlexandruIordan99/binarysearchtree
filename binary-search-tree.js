08df83239856e1fe33834f45fb1559d2df841483
alexandruiordan99 <alexandru.iordan77@gmail.com>
2025-05-26 13:23:12 +0200

    Initial BST setup

---
+++ binary-search-tree.js
@@ -0,0 +1,73 @@
+class binarySearchTree {
+  constructor(root, array) {
+    this.root = root;
+    this.array = array;
+  }
+
+  buildTree(array) {
+
+  }
+
+  insert(value) {
+
+  }
+
+  deleteItem(value) {
+
+  }
+
+  find(value) {
+
+  }
+
+  levelOrder(callback){
+
+  }
+
+  inOrder(callback){
+
+  }
+
+  preOrder(callback){
+
+  }
+
+  postOrder(callback){
+
+  }
+
+  height(value){
+
+  }
+
+  depth(value){
+
+  }
+
+  isBalanced(){
+
+  }
+
+  rebalance(){
+    if (this.isBalanced()){
+      return "Tree is already balanced";
+    }
+
+  }
+
+
+  prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+  };
+
+}
+
---
+++ node.js
@@ -0,0 +1,13 @@
+class node{
+  constructor(value, leftChild, rightChild){
+    this.value = value;
+    this.leftChild = leftChild;
+    this.rightChild = rightChild;
+  }
+
+}
+
+
+
+
+module.exports = {node};
