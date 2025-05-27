2a07848658a8ccc00256f732303c7f05398eed4d
alexandruiordan99 <alexandru.iordan77@gmail.com>
2025-05-27 15:13:56 +0200

    Uploading driver

---
+++ driver.js
@@ -0,0 +1,9 @@
+import {BinarySearchTree} from './binary-search-tree.js';
+
+const tree = new BinarySearchTree([4, 5, 8, 9, 12, 54, 22, 11, 14, 17]);
+
+tree.insert(77);
+tree.insert(78);
+tree.insert(79);
+tree.insert(80);
+tree.prettyPrint();
