9a593742dfbe1433c8c3fb36171f271d746d97af
alexandruiordan99 <alexandru.iordan77@gmail.com>
2025-05-27 12:20:48 +0200

    Added insert, delete and find functions

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -6,14 +6,86 @@ class binarySearchTree {
   }
 
   insert(value) {
+    let root = this.root;
 
+    const addValue = function(root, value) {
+      if (root === null){
+        return new Node(value);
+      }
+      if (root === value) {
+        return root;
+      } else if (value < root.data) {
+        root.left = addValue(root.left, value);
+      } else {
+        root.right = addValue(root.right, value);
+      }
+      return addValue(value);
+    }
   }
 
   deleteItem(value) {
+    let root = this.root;
+    let data = null;
 
+    const findNode = function(root, value) {
+      if(root === null){
+        return null;
+      }
+
+      if(root.data ===value){
+        if (root.left ===null && root.right===null){
+          return null;
+        } else if (root.left !== null && root.right !== null){
+          root.data = successorNode(root, value);
+          root.right = findNode(root.right, data);
+        } else if (root.left !== null){
+          return root.left;
+        } else if (root.right !== null){
+          return root.right;
+        }
+
+      } else if (root.data > value){
+        root.left = findNode(root.left, value);
+      } else {
+        root.right = findNode(root.right, value);
+      }
+      return root;
+
+    }
+
+    const successorNode = function(root, value) {
+      if (root.left ===null){
+        return root.data;
+      }
+      if (root.data > value){
+        data = successorNode(root.left, value);
+      } else {
+        data = successorNode(root.right, value);
+      }
+      return data;
+    }
+    return findNode(root, value);
   }
 
   find(value) {
+    let root = this.root;
+
+    const findNode = function(root, value) {
+      if (root === null){
+        return null;
+      }
+
+      if (root.data === value){
+        return root;
+      } else if (root.data > value){
+        root = findNode(root.right, value);
+      } else {
+        root =  findNode(root.right, value);
+      }
+      return root;
+    }
+
+    return findNode(root.left, value);
 
   }
 
